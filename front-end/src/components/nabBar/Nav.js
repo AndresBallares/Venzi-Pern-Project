20f0783d9dc850d64d220a4a35bee5a6050c93cf
andres ballares <andr3sb@hotmail.com>
2022-02-24 17:23:38 -0500

    final commit

---
+++ front-end/src/components/nabBar/Nav.js
@@ -0,0 +1,20 @@
+
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+
+const NavBar = () => {
+        return (
+            <nav className="navbar">
+                <img src="file:///Users/maurolozano/Pictures/Photos%20Library.photoslibrary/private/com.apple.Photos" alt="logo" />
+                    
+                <button className="button">
+                <Link to="/jewelry/CreateJewelry">Customize</Link>
+                </button>
+                
+            </nav>
+        )
+
+} 
+
+export default NavBar;
